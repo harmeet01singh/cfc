0599abf507c00a575e03539d2ea6944cb2da62be
RohanpolGIT <RohanpolGIT@users.noreply.github.com>
2020-11-18 10:31:14 +0530

    all commit

---
+++ frontend/src/Components/Users/Contribute/handin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function handin() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ frontend/src/Components/Users/Eve/past.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function past() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
