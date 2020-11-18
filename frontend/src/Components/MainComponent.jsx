506c2d298d766b0b931074146f3e57b38a021964
RohanpolGIT <rohanpol36@gmail.com>
2020-11-18 12:06:35 +0530

    financials,newletter and Header

--- frontend/src/Components/MainComponent.jsx
+++ frontend/src/Components/MainComponent.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import Header from './User/Header';
+import Newsletter from './User/Aboutus/Newsletter';
+import Financials from './User/Aboutus/Financials';
 
 class MainComponent extends React.Component {
     render(){
         return (
             <div>
                 <Header/>
-    
+                {/*<Newsletter/>*/}
+                <Financials/>
             </div>
         )
     }
--- frontend/src/Components/User/Aboutus/Newsletter.jsx
+++ frontend/src/Components/User/Aboutus/Newsletter.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Row, Col,
+  } from 'reactstrap';
 
 export default function Newsletter() {
     return (
-        <div>
-            
+        <div className="container">
+            <br></br>
+            <Row>
+                <Col sm="4">
+                    <Card body>
+                    <CardTitle tag="h5">Newsletter December</CardTitle>
+                    <CardText></CardText>
+                    <Button>Get Newsletter</Button>
+                    </Card>
+                </Col>
+                <Col sm="4">
+                    <Card body>
+                    <CardTitle tag="h5">Newsletter November</CardTitle>
+                    <CardText></CardText>
+                    <Button>Get Newsletter</Button>
+                    </Card>
+                </Col>
+                <Col sm="4">
+                    <Card body>
+                    <CardTitle tag="h5">Newsletter October</CardTitle>
+                    <CardText></CardText>
+                    <Button>Get Newsletter</Button>
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
